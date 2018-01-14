c046f63c908b4deecd56cd371749861544f5802e
Johannes C. Mayer <j.c.mayer240@gmail.com>
2018-01-14 20:20:38 +0100

    finished test with random cv search on SVC

--- CleanOutput.sh
+++ CleanOutput.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-for filename in submission_data*.csv; do
-    rm filename;
+ARRAY=(./submission_data*.csv, Trained_*_Data)
+
+for filesig in ARRAY; do
+  for filename in ./submission_data*.csv; do
+    rm ${filename};
     done
+  done
 
 exit 0
--- Main.py
+++ Main.py
@@ -1,15 +1,34 @@
+import subprocess as sp
 import pandas as pd
 import numpy as np
+import os
+
+from sklearn.externals import joblib
+from random import uniform
+
 from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.preprocessing import StandardScaler, Imputer
-from sklearn.model_selection import StratifiedShuffleSplit, cross_val_score
+from sklearn.model_selection import StratifiedShuffleSplit, cross_val_score, RandomizedSearchCV
 from sklearn.base import TransformerMixin, BaseEstimator
 
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.svm import SVC, LinearSVC, NuSVC
 from sklearn.tree import DecisionTreeClassifier, ExtraTreeClassifier
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
 from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+
+def project_setup(saved_models_path='data/', saved_predictions_path='predictions/'):
+    global paths
+    paths = {
+        'saved_models_path': saved_models_path,
+        'saved_predictions_path': saved_predictions_path,
+    }
+
+    for _, path in paths.items():
+        if not os.path.exists(path):
+            os.makedirs(path)
 
 
 def split_data(data, test_size=0.2):
@@ -31,7 +50,7 @@ class DataFrameSelector(TransformerMixin, BaseEstimator):
         return X[self.atribs].values
 
 
-def transform_data(data, num_atribs, cat_atribs):
+def transform_data(data, num_atribs, cat_atribs, no_fit=False):
     # Feature Engenering
     pd.set_option('chained_assignment', None)
     data.loc[:, ("Fare_Pclass")] = data["Fare"] / (np.power(data["Pclass"], 2))
@@ -50,19 +69,26 @@ def transform_data(data, num_atribs, cat_atribs):
         ('std_scaler', StandardScaler()),
     ])
 
-    transformed_data = pd.concat([pd.DataFrame(num_pipeline.fit_transform(data), columns=num_atribs), one_hot_cat_data],
+    if no_fit:
+        num_pipeline = joblib.load('{}num_pipe'.format(paths.get('saved_models_path')))
+    else:
+        num_pipeline.fit(data)
+        joblib.dump(num_pipeline, '{}num_pipe'.format(paths.get('saved_models_path')))
+
+    transformed_data = pd.concat([pd.DataFrame(num_pipeline.transform(data), columns=num_atribs), one_hot_cat_data],
                                  axis=1)
+
     transformed_data.loc[:, ('PassengerId')] = saved_passengerId.values
+
     return pd.DataFrame(transformed_data)
 
+def drop_passenger_iD(X):
+    saved_passengerId = X['PassengerId']
+    X = X.drop(['PassengerId'], axis=1)
+    return X, saved_passengerId
 
-def run_model(model, train_data, train_data_lables, prediction_data):
+def run_model(model, train_data, train_data_lables, prediction_data, no_fit=False):
     print('Model: {}'.format(type(model).__name__))
-
-    def drop_passenger_iD(X):
-        saved_passengerId = X['PassengerId']
-        X = X.drop(['PassengerId'], axis=1)
-        return X, saved_passengerId
 
     train_data, saved_pasid_train = drop_passenger_iD(train_data)
     prediction_data, saved_pasid_pred = drop_passenger_iD(prediction_data)
@@ -71,8 +97,13 @@ def run_model(model, train_data, train_data_lables, prediction_data):
     scores = cross_val_score(model, train_data, train_data_lables, cv=4)
     print('Cross-validation score average: {}%'.format(truncate(sum(scores) / len(scores) * 100, 2)))
 
-    # Test performance on Train data to detect potential overfitting
-    model.fit(train_data, train_data_lables)
+    # Make predictions
+    if no_fit:
+        model = joblib.load('{}Trained_{}_Data'.format(paths.get('saved_models_path'), type(model).__name__))
+    else:
+        model.fit(train_data, train_data_lables)
+        joblib.dump(model, '{}Trained_{}_Data'.format(paths.get('saved_models_path'), type(model).__name__))
+
     pred_on_train_data = model.predict(train_data) == train_data_lables
     print('Acuracy on training data {}%\n'.format(
         truncate(len([p for p in pred_on_train_data if p == True]) / len(pred_on_train_data) * 100, 2)))
@@ -94,30 +125,69 @@ def run():
 
     transformed_train_data = transform_data(train_data, num_atribs=num_atribs, cat_atribs=cat_atribs)
     transformed_test_data = transform_data(test_data, num_atribs=num_atribs, cat_atribs=cat_atribs)
-    transformed_submission_data = transform_data(submission_data, num_atribs=num_atribs, cat_atribs=cat_atribs)
+    transformed_submission_data = transform_data(submission_data, num_atribs=num_atribs, cat_atribs=cat_atribs,
+                                                 no_fit=True)
 
     train_data_labels = train_data['Survived']
     test_data_labels = test_data['Survived']
 
     models = {
-            0: RandomForestClassifier(max_depth=None, max_leaf_nodes=None, warm_start=True),
-            1: LinearSVC(),
-            2: NuSVC(),
-            3: SVC(C=1.0),
-            4: DecisionTreeClassifier(),
-            5: ExtraTreeClassifier(),
-        }
+        0: RandomForestClassifier(max_depth=None, max_leaf_nodes=None, warm_start=True),
+        1: LinearSVC(),
+        2: NuSVC(),
+        3: SVC(C=1.0),
+        4: DecisionTreeClassifier(),
+        5: ExtraTreeClassifier(),
+        6: GaussianNB(),
+        7: KNeighborsClassifier(),
+        8: MLPClassifier(max_iter=1000),
+        9: AdaBoostClassifier(),
+    }
     model_num_single_model = 3
 
-    if model_num_single_model is None:
-        for _, model in models.items():
-            #prediction_on_train_split = run_model(model, transformed_train_data, train_data_labels, transformed_test_data)
-            submission_prediction = run_model(model, transformed_train_data, train_data_labels, transformed_submission_data)
-            submission_prediction.to_csv('submission_data_{}.csv'.format(type(model).__name__), index=False)
-    else:
-        model = models[model_num_single_model]
-        submission_prediction = run_model(model, transformed_train_data, train_data_labels, transformed_submission_data)
-        submission_prediction.to_csv('submission_data_{}.csv'.format(type(model).__name__), index=False)
+    param_dist = {
+        'C': list(range(1, 15)),
+        'kernel': ['rbf', 'sigmoid'], #['rbf'], 'poly', 'linear', 'sigmoid', 'precomputed'],
+        'degree': [3],
+        'gamma': ['auto'],
+        'coef0': [0.0],
+        'shrinking': [True],
+        'probability': [False],
+        'tol': [1e-3],
+        'cache_size': list(range(1, 2000)),
+        'class_weight': [None],
+        'verbose': [False],
+        'max_iter': [-1],
+        'decision_function_shape': ['ovr'],
+        'random_state': [42]
+    }
+
+    transformed_train_data, saved_pasid_train = drop_passenger_iD(transformed_train_data)
+    transformed_submission_data, saved_pasid_pred = drop_passenger_iD(transformed_submission_data)
+
+    rand_search = RandomizedSearchCV(models[model_num_single_model], n_iter=10000, param_distributions=param_dist)
+    rand_search.fit(transformed_train_data, train_data_labels)
+    print(rand_search.best_estimator_)
+    submission_prediction = rand_search.predict(transformed_submission_data)
+
+    df = pd.DataFrame()
+    df['PassengerId'] = saved_pasid_pred
+    df['Survived'] = submission_prediction
+
+    df.to_csv(
+        '{}submission_data_randcv_SVC.csv'.format(paths.get('saved_predictions_path')), index=False)
+
+    # if model_num_single_model is None:
+    #     for _, model in models.items():
+    #         # prediction_on_train_split = run_model(model, transformed_train_data, train_data_labels, transformed_test_data)
+    #         submission_prediction = run_model(model, transformed_train_data, train_data_labels,
+    #                                           transformed_submission_data)
+    #         submission_prediction.to_csv('{}submission_data_{}.csv'.format(paths.get('saved_predictions_path'), type(model).__name__), index=False)
+    # else:
+    #     model = models[model_num_single_model]
+    #     submission_prediction = run_model(model, transformed_train_data, train_data_labels,
+    #                                       transformed_submission_data)
+    #     submission_prediction.to_csv('{}submission_data_{}.csv'.format(paths.get('saved_predictions_path'), type(model).__name__), index=False)
 
 
 # Utilities
@@ -132,6 +202,5 @@ def truncate(f, n):
 
 if __name__ == '__main__':
     print('\n')
+    project_setup()
     run()
-
-
